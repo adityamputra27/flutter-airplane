e021e2bf24edc8b1cdea1b2d0fb088cc00744b5d
dittmptr27 <adityamuhamadputra@gmail.com>
2023-04-27 05:42:30 +0700

    chore: integrated flutter_bloc

--- lib/ui/pages/setting_page.dart
+++ lib/ui/pages/setting_page.dart
@@ -1,7 +1,5 @@
 import 'package:airplane/shared/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
